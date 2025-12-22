d56e4c7334f9df04c7091d9dd0f30a8ee120c6b8
Mohammad Alhnani <145880506+WWW-Alhnani-COM@users.noreply.github.com>
2025-12-23 00:56:21 +0300

    Create gtag.js

---
+++ gtag.js
@@ -0,0 +1,9 @@
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=AW-17824491699"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'AW-17824491699');
+</script>
