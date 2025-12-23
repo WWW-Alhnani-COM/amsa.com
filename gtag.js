c3d87e907546ea1cfefca1d9b616d0b66bdff041
Mohammad Alhnani <145880506+WWW-Alhnani-COM@users.noreply.github.com>
2025-12-23 06:03:40 +0300

    Update gtag.js

--- gtag.js
+++ gtag.js
@@ -1,20 +1,9 @@
-<!—Google tag (gtag.js) 
-
+<!-- Google tag (gtag.js) -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=AW-17824491699"></script>
-
 <script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
 
-  Window.dataLayer = window.dataLayer || [];
-
-  Function gtag(){dataLayer.push(arguments);}
-
-  Gtag('js', new Date());
-
-
-
-  Gtag('config', 'AW-17824491699');
-
+  gtag('config', 'AW-17824491699');
 </script>
-
-
-
